202d459b7d44f22e245f0eca853ada3a58494564
Unknown <p.jae21@hotmail.com>
2018-02-13 23:54:07 +0000

    Data storage implemented by using NSCoder

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -12,56 +12,12 @@ class ToDoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
-//            itemArray = items
-//        }
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
         
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3)
-        
-        let newItem4 = Item()
-        newItem4.title = "Destroy Demogorgon 1"
-        itemArray.append(newItem4)
-        
-        let newItem5 = Item()
-        newItem5.title = "Destroy Demogorgon 2"
-        itemArray.append(newItem5)
-        
-        let newItem6 = Item()
-        newItem6.title = "Destroy Demogorgon 3"
-        itemArray.append(newItem6)
-        
-        let newItem7 = Item()
-        newItem7.title = "Destroy Demogorgon 4"
-        itemArray.append(newItem7)
-        
-        let newItem8 = Item()
-        newItem8.title = "Destroy Demogorgon 5"
-        itemArray.append(newItem8)
-        
-        let newItem9 = Item()
-        newItem9.title = "Destroy Demogorgon 6"
-        itemArray.append(newItem9)
-        
-        let newItem10 = Item()
-        newItem10.title = "Destroy Demogorgon 7"
-        itemArray.append(newItem10)
-        
-        if let items = defaults.array(forKey: "ToDoListArray") as? [Item]{
-            itemArray = items
-        }
+        loadItems()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -83,6 +39,8 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
+        saveItems()
+        
         tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
@@ -101,7 +59,9 @@ class ToDoListViewController: UITableViewController {
             newItem.title = textField.text!
             
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
+            self.saveItems()
+            
             self.tableView.reloadData()
         }
         
@@ -113,6 +73,28 @@ class ToDoListViewController: UITableViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+    }
+    
+    func loadItems(){
+        if let data = try? Data.init(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decodinf item array, \(error)")
+            }
+        }
     }
 }
 
